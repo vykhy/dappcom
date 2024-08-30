a3096df8bd4fbfbe6794b0a4a02e3e2cf91dfa84
vykhy <vikehietoyhoshii@gmail.com>
2024-08-30 10:17:18 +0530

    buy product

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,9 +1,43 @@
 import { ethers } from "ethers";
 import close from "./../assets/close.svg";
 import Rating from "./Rating";
+import { useEffect, useState } from "react";
 
 function Product({ provider, item, dappcom, togglePop, account }) {
-  async function buyHandler() {}
+  const [order, setOrder] = useState(null);
+  const [orderCount, setOrderCount] = useState(0);
+  const [hasBought, setHasBought] = useState(false);
+
+  const fetchDetails = async () => {
+    const events = await dappcom.queryFilter("Buy");
+    const orders = events.filter(
+      (event) =>
+        event.args.buyer === account &&
+        event.args.itemId.toString() === item.id.toString()
+    );
+    if (orders.length === 0) return;
+
+    const order = await dappcom.orders(account, orders[0].args.orderId);
+    setOrderCount(orders.length);
+    setOrder(order);
+    // setHasBought(true);
+  };
+
+  async function buyHandler() {
+    const signer = await provider.getSigner();
+
+    let transaction = await dappcom
+      .connect(signer)
+      .buy(item.id, { value: item.cost });
+
+    await transaction.wait();
+    setHasBought(true);
+  }
+
+  useEffect(() => {
+    fetchDetails();
+  }, [hasBought]);
+
   return (
     <div className="product">
       <div className="product__details">
@@ -46,6 +80,21 @@ function Product({ provider, item, dappcom, togglePop, account }) {
           <p>
             <small>Sold by</small> Dappcom
           </p>
+          {order && (
+            <div className="product__bought">
+              Item bought {orderCount} times. First bought on <br />
+              <strong>
+                {new Date(
+                  Number(order.time.toString() + "000")
+                ).toLocaleDateString(undefined, {
+                  weekday: "long",
+                  hour: "numeric",
+                  minute: "numeric",
+                  second: "numeric",
+                })}
+              </strong>
+            </div>
+          )}
         </div>
         <button onClick={() => togglePop(null)} className="product__close">
           <img src={close} alt="Close" />
